0fabc7a3077e63aa8aaa852fb95e307e4e23563e
ngmandyn <ngmandyn@sfu.ca>
2018-02-01 22:27:58 -0800

    copypasta operations for 7 dimensions

--- loadData.js
+++ loadData.js
@@ -1,20 +1,34 @@
 var url = "http://www.sfu.ca/~ngmandyn/iat355/All_GPUs.csv";
-
-var gpuData = d3.csv(url);
-
 var parseEmptyRegex = /\w+/;
+var dimensions = ['Core_Speed', 'Max_Power', 'Memory', 'Memory_Bandwidth', 'Memory_Speed', 'PSU', 'Texture_Rate'];
+var units = ['MHz', 'Watts', 'MB', 'GB/sec', 'MHz', 'Watts', 'GTexel/s'];
 
 d3.csv(url, prepData, function(data) {
-  console.log("Total GPUs: " + data.length)
-  console.log("Max: " + getMax(data, 'Core_Speed'));
-  console.log("Min: " + getMin(data, 'Core_Speed'));
-  console.log("Sum: " + getSum(data, 'Core_Speed'));
-  console.log("Average: " + getMean(data, 'Core_Speed'));
-  console.log("Count: " + getCount(data, 'Core_Speed'));
+  console.log("TOTAL DATA ITEMS: " + data.length)
+  // calculate maximum values for 7 dimensions
+  for (i = 0; i < dimensions.length; i++) {
+    console.log("Max " + dimensions[i] + " (" + units[i] + "): " + getMax(data, dimensions[i]));
+  }
+  // calculate minimum values for 7 dimensions
+  for (i = 0; i < dimensions.length; i++) {
+    console.log("Min " + dimensions[i] + " (" + units[i] + "): " + getMin(data, dimensions[i]));
+  }
+  // calculate sum for 7 dimensions
+  for (i = 0; i < dimensions.length; i++) {
+    console.log("Sum - " + dimensions[i] + " (" + units[i] + "): " + getSum(data, dimensions[i]));
+  }
+  // calculate mean for 7 dimensions
+  for (i = 0; i < dimensions.length; i++) {
+    console.log("Average " + dimensions[i] + " (" + units[i] + "): " + getMean(data, dimensions[i]));
+  }
+  // calculate number of records for 7 dimensions
+  for (i = 0; i < dimensions.length; i++) {
+    console.log("Count - " + dimensions[i] + " (" + units[i] + "): " + getCount(data, dimensions[i]));
+  }
 })
 
 function prepData(data) {
-  stripUnitsForColumns(data, ['Core_Speed', 'ROPs']);
+  stripUnitsForColumns(data, dimensions);
   return data;
 }
 
